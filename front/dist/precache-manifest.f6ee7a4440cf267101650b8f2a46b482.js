b7af511245a99c65baf5918ec0866be8236790cd
yzl <yangzhenlie@banggood.com>
2018-12-30 13:27:51 +0800

    add

---
+++ front/dist/precache-manifest.f6ee7a4440cf267101650b8f2a46b482.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "14d6338d523cc8e1dc6a",
+    "url": "/js/chunk-2d636d39.78886569.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "300d70cfb6f60f332173",
+    "url": "/css/chunk-6d8d19b8.6ae192e6.css"
+  },
+  {
+    "revision": "dfd1f279fb931642287d",
+    "url": "/css/chunk-1ffb2741.63f31384.css"
+  },
+  {
+    "revision": "0387fd9c3a5e2961559f",
+    "url": "/css/chunk-232be0b8.a0459b18.css"
+  },
+  {
+    "revision": "0387fd9c3a5e2961559f",
+    "url": "/js/chunk-232be0b8.d5968406.js"
+  },
+  {
+    "revision": "14d6338d523cc8e1dc6a",
+    "url": "/css/chunk-2d636d39.3c1c3440.css"
+  },
+  {
+    "revision": "03d9285082547edb1bbb",
+    "url": "/js/app.41593c13.js"
+  },
+  {
+    "revision": "dfd1f279fb931642287d",
+    "url": "/js/chunk-1ffb2741.9040df82.js"
+  },
+  {
+    "revision": "300d70cfb6f60f332173",
+    "url": "/js/chunk-6d8d19b8.08e9b9ae.js"
+  },
+  {
+    "revision": "75a54e9d371ff8219fd4",
+    "url": "/css/chunk-a77bc028.3ea74962.css"
+  },
+  {
+    "revision": "75a54e9d371ff8219fd4",
+    "url": "/js/chunk-a77bc028.a57e8a38.js"
+  },
+  {
+    "revision": "3525340b5662612ec745",
+    "url": "/js/chunk-vendors.849151db.js"
+  },
+  {
+    "revision": "7cb7bb954e2dcc723c9f846caa20447d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03d9285082547edb1bbb",
+    "url": "/css/app.22c4d300.css"
+  }
+];
